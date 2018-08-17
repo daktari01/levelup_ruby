69fe0c65cd9df75b05df5165fbbf101d61844e94
James Dindi <dindijjames@gmail.com>
2018-08-17 21:46:56 +0300

    Instance variables and methods

--- 02_classes_and_objects/classes.rb
+++ 02_classes_and_objects/classes.rb
@@ -1,7 +1,21 @@
 class Spaceship
     # Classes begin with caps and use camelCase
-    
+    def launch(destination)
+        # Go towards destination
+        @destination = destination
+    end
+    # Define a method to access the instance variable @destination
+    # Instance variables are private and can only be accessed via methods
+    def destination
+        @destination
+    end
 end
 
 # Class object
 ship = Spaceship.new
+ship.launch("Earth")
+# Print out info about the object
+puts ship.inspect
+# This also prints out info about the object
+p ship
+puts ship.destination
